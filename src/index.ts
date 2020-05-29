a14b3891f5d45bc7d616f81c14dcb8645895657d
taioo <t.aldefai@outlook.com>
2020-05-29 21:45:11 +0200

    set some REST methods

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
  
+
+dotenv.config();
+
+if (!process.env.PORT) {
+   process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+
 const app = express();
+app.use(cors());
+app.use(express.json());
  
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  return res.send('Hello World!');
 });
  
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+ 
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+ 
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+ 
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
 );
