9f1f762cc549a44af01742b4b62f254773256913
taioo <t.aldefai@outlook.com>
2020-05-31 00:14:20 +0200

    add graphql & appollo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import dotenv from 'dotenv'
 import express from 'express'
-import cors from 'cors'
-
-dotenv.config()
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 
 dotenv.config()
 
@@ -13,28 +13,51 @@ if (!process.env.PORT) {
 
 const PORT: number = parseInt(process.env.PORT as string, 10)
 
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] , hello: String}
+  type Book { title: String, author: String }
+`
+// The resolvers
+const resolvers = {
+  Query: { hello: () => 'Hello World!', books: () => books }
+}
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
 const app = express()
-app.use(cors())
-app.use(express())
-
-// curl -X GET http://localhost:3000
-
-app.get('/', (req: any, res: any) => {
-  return res.send('Hello World!')
-})
-
-app.post('/', (req: any, res: any) => {
-  return res.send('Received a POST HTTP method')
-})
-
-app.put('/', (req: any, res: any) => {
-  return res.send('Received a PUT HTTP method')
-})
-
-app.delete('/', (req: any, res: any) => {
-  return res.send('Received a DELETE HTTP method')
-})
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`)
+// The GraphQL endpoint
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress(req => {
+    return {
+      schema: schema,
+      context: {
+        value: req
+      }
+      // other options here
+    }
+  })
 )
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+app.listen(PORT, () => { console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`) })
