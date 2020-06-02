c476900c67a3cfde1d9c0e122f43a0b9c9461a39
taioo <t.aldefai@outlook.com>
2020-06-02 18:06:16 +0200

    add scalar Date

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { User } from './entity/User'
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
+import * as Moment from 'moment'
 
 // Provide resolver functions for your schema fields
 export const resolvers = {
@@ -45,5 +48,21 @@ export const resolvers = {
         return false
       }
     }
-  }
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Custom description for the date scalar',
+    parseValue (value) {
+      return Moment(value) // value from the client
+    },
+    serialize (value) {
+      return Moment(value).format() // value sent to the client
+    },
+    parseLiteral (ast) {
+      if (ast.kind === Kind.STRING) {
+        return Moment(ast.value) // ast value is always in string format
+      }
+      return null
+    }
+  })
 }
